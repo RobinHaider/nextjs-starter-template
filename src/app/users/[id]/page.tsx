fd43500e314a6a19da4adf3b8de402d640159026
Robin Haider <robinhaider69@gmail.com>
2023-06-20 14:31:57 +0600

    load page with suspence

--- src/app/users/[id]/page.tsx
+++ src/app/users/[id]/page.tsx
@@ -1,6 +1,7 @@
 import getUser from '@/lib/getUser';
 import getUserPost from '@/lib/getUserPost';
-import React from 'react';
+import React, { Suspense } from 'react';
+import UserPosts from './components/UserPosts';
 
 type Params = {
   params: {
@@ -12,16 +13,31 @@ export default async function UserPage({ params: { id } }: Params) {
   const userData: Promise<User> = getUser(id);
   const userPostsData: Promise<Post[]> = getUserPost(id);
 
-  const [user, userPosts] = await Promise.all([userData, userPostsData]);
+  // parallel fetching
+  // const [user, userPosts] = await Promise.all([userData, userPostsData]);
+
+  // return (
+  //   <div>
+  //     <h1>{user.name}</h1>
+  //     <ul>
+  //       {userPosts.map((post) => (
+  //         <li key={post.id}>{post.title}</li>
+  //       ))}
+  //     </ul>
+  //   </div>
+  // );
+
+  // sequential fetching
+  const user = await userData;
 
   return (
     <div>
       <h1>{user.name}</h1>
-      <ul>
-        {userPosts.map((post) => (
-          <li key={post.id}>{post.title}</li>
-        ))}
-      </ul>
+      <br />
+      <h2>Posts</h2>
+      <Suspense fallback={<div>Loading...</div>}>
+        <UserPosts promise={userPostsData} />
+      </Suspense>
     </div>
   );
 }
