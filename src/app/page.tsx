d67ac7d34a9daf5fea61366e8547f74f1f8d1351
Robin Haider <robinhaider69@gmail.com>
2023-06-21 17:24:23 +0600

    nav component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 export default function Home() {
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <h1 className='text-6xl font-bold text-center'>
-        Welcome to <a href='https://nextjs.org'>Next.js!</a>
+      <h1 className='text-5xl font-bold text-center'>
+        Welcome to <a href='https://nextjs.org'>Next.js Starter Template!</a>
       </h1>
+      <p className='text-gray-500'>The Ultimate Starter Template</p>
     </main>
   );
 }
